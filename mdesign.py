a7b81c4e63ce5d1ff28015ca22dd58d495d193f0
cj <cjokee@gmail.com>
2024-01-15 18:27:05 +0100

    refactoring
    UserDialog and added dataclass to make it easier to add new commands.

--- mdesign.py
+++ mdesign.py
@@ -6,56 +6,87 @@ Author:     Allan Olsen aka cjoke
 Purpose:    Create a mindmap application with graphviz and save as dot file.
 license:    MIT License (see LICENSE online)
 """
+
+
 import graphviz as gv
-import os
+from dataclasses import dataclass
+from typing import Callable, List
 
-class Mindmap:
-    def __init__(self, name):
-        self.name = name
-        self.graph = gv.Digraph(format='dot')
-        self.graph.attr('node', shape='box')
 
-    def add_node(self, node):
+@dataclass
+class Command:
+    """
+    This class represents a command that the user can execute.
+    """
+
+    name: str
+    description: str
+    action: Callable[[], None]
+
+
+@dataclass
+class UserDialog:
+    """
+    This class is responsible for processing user input.
+    """
+
+    graph: gv.Digraph
+    commands: List[Command]
+
+    def __init__(self, graph):
+        self.graph = graph
+        self.commands = self.initialize_commands()
+
+    def initialize_commands(self) -> List[Command]:
+        return [
+            Command("1", "Add node", self.add_node),
+            Command("2", "Remove node", self.remove_node),
+            Command("3", "Add edge", self.add_edge),
+            Command("4", "View nodes", self.view_nodes),
+            Command("5", "Save graph", self.save_graph),
+            Command("6", "Exit", self.exit),
+        ]
+
+    def add_node(self):
+        node = input("Enter the name of the node you want to add: ")
         self.graph.node(node)
 
-    def remove_node(self, node):
-        self.graph.node(node, _attributes={'style': 'invis'})
+    def remove_node(self):
+        # Graphviz does not support removing nodes directly.
+        # You might need to create a new graph without the node to remove it.
+        pass
 
-    def add_edge(self, edge, label=None):
-        if label:
-            self.graph.edge(edge[0], edge[1], label=label)
-        else:
-            self.graph.edge(edge[0], edge[1])
-
-    def save(self):
-        self.graph.render(self.name, view=True)
-        os.remove(self.name)
-
-class UserDialog:
-    def __init__(self, mindmap):
-        self.mindmap = mindmap
+    def add_edge(self):
+        edge = input(
+            "Enter the names of the two nodes you want to connect, separated by a comma: "
+        ).split(",")
+        self.graph.edge(edge[0].strip(), edge[1].strip())
 
     def view_nodes(self):
-        return self.mindmap.graph.body
+        print(self.graph.source)
 
-    def add_node(self, node):
-        self.mindmap.add_node(node)
+    def save_graph(self):
+        self.graph.render("graph", view=True)
 
-    def remove_node(self, node):
-        self.mindmap.remove_node(node)
+    def exit(self):
+        self.save_graph()
+        exit()
 
-    def add_edge(self, edge, label=None):
-        self.mindmap.add_edge(edge, label)
+    def start(self):
+        while True:
+            for command in self.commands:
+                print(f"{command.name}. {command.description}")
+            choice = input("Choose an option: ")
 
-if __name__ == '__main__':
-    m = Mindmap('test')
-    u = UserDialog(m)
-    u.add_node('a')
-    u.add_node('b')
-    u.add_node('c')
-    u.add_edge(('b', 'a'), label='pointer')
-    u.add_edge(('b', 'c'), label='pointer')
-    u.add_edge(('a', 'b'), label='pointer')
-    u.add_edge(('a', 'c'), label='pointer')
-    print(u.view_nodes())
-    m.save()
+            for command in self.commands:
+                if choice == command.name:
+                    command.action()
+                    break
+            else:
+                print("Invalid option. Please try again.")
+
+
+if __name__ == "__main__":
+    m = gv.Digraph(format="dot")
+    d = UserDialog(m)
+    d.start()
